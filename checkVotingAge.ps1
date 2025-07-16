dadc730f31ba1d2d0f2b670010238e5a3493528e
Rohan-NPower-Git-Repo <rohan.deshpande@npowercanada.ca>
2025-07-16 15:54:51 -0400

    Added Comment for If and else both conditions

--- checkVotingAge.ps1
+++ checkVotingAge.ps1
@@ -1,13 +1,15 @@
 # Typecast The Command Line Arguments to the Data-type we want
+
 param (
-    [string]$uName,
-    [int]$uAge
+	[string]$userName,
+	[int]$userAge
 )
 
-if ($uAge -ge 18) {
-    Write-Host "$uName who is $uAge years old is eligible to vote"
+if( $userAge -ge 18 ) {
+	Write-Host "$userName who is $userAge years old is eligible to Vote"
 } else {
-    Write-Host "$uName who is $uAge years old is NOT eligible to vote"
+	Write-Host "$userName who is $userAge years old is NOT eligible to Vote"
 }
 
-# Run As : .\checkVotingAge.ps1 -uName "John Doe" -uAge 20
+# .\checkVotingAge.ps1 -userName "John Doe" -userAge 25
+# .\checkVotingAge.ps1 -userName "Jane Doe" -userAge 16
